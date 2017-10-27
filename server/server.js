121a9e996e6ea37fb9c611b31a1a728d28f661ef
Dez <elye24@gmail.com>
2017-10-27 11:27:46 +0200

    cross origin request

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const {mongoose} = require('./db/mongoose')
 const {Anime} = require('./models/anime')
 
 let app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const port = process.env.PORT
 
 app.use(bodyParser.json()) // set bodyParser middleware

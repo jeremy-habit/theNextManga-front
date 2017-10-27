dd8e9a66a35e0722a07f5b8b9ddda17732c65441
Jeremy Habit <jeremy.habit@gmail.com>
2017-10-27 11:17:11 +0200

    cosmos...

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 window.onload = function () {
 
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
   /* @@@ DATAS  @@@ */
-  var mangas = [
-    {
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+  let mangas = [
+    /*{
       "a": "noblesse",
       "c": [
         "Action",
@@ -11,9 +16,16 @@ window.onload = function () {
         "Mystery",
         "School Life",
         "Shounen",
-        "Supernatural"
+        "Supernatural",
+        "Cosmos"
       ],
-      "h": 154860989,
+      "c_common": [
+        "Action",
+        "Comedy",
+        "Cosmos",
+        "Drama"
+      ],
+      "h": 150,
       "i": "4e70ea03c092255ef70046ea",
       "im": null,
       "ld": 1507621073.0,
@@ -29,6 +41,10 @@ window.onload = function () {
         "Drama",
         "Fantasy",
         "Shounen"
+      ],
+      "c_common": [
+        "Adventure",
+        "Drama"
       ],
       "h": 251208555,
       "i": "4e70ea03c092255ef70046f0",
@@ -47,6 +63,13 @@ window.onload = function () {
         "Romance",
         "Slice of Life"
       ],
+      "c_common": [
+        "Action",
+        "Comedy",
+        "Cosmos",
+        "Drama",
+        "test"
+      ],
       "h": 465456649,
       "i": "4e70e9dbc092255ef7003c77",
       "im": "7f/7fff5bd64695f4a4d37277166855194ffe90dc75603774b7580e3c1f.jpg",
@@ -59,7 +82,11 @@ window.onload = function () {
       "c": [
         "Comedy",
         "Romance",
-        "Yaoi"
+        "Yaoi",
+        "Cosmos"
+      ],
+      "c_common": [
+        "Cosmos"
       ],
       "h": 131012122,
       "i": "4e70ea08c092255ef700484d",
@@ -76,6 +103,9 @@ window.onload = function () {
         "Romance",
         "Sci-fi",
         "Seinen"
+      ],
+      "c_common": [
+        "Drama"
       ],
       "h": 112750152,
       "i": "4e70ea1ec092255ef7004dac",
@@ -95,6 +125,9 @@ window.onload = function () {
         "Seinen",
         "Supernatural"
       ],
+      "c_common": [
+        "Drama"
+      ],
       "h": 595402456,
       "i": "521e575345b9efc819f90d26",
       "im": "6c/6c6af25b8a6316078c52d15294940329b50c05cd1d9a8a04515559f5.jpg",
@@ -108,6 +141,9 @@ window.onload = function () {
         "Drama",
         "Romance",
         "Yaoi"
+      ],
+      "c_common": [
+        "Drama"
       ],
       "h": 115457607,
       "i": "4e70e98ec092255ef7002ca5",
@@ -124,6 +160,9 @@ window.onload = function () {
         "Mecha",
         "Sci-fi",
         "Shounen"
+      ],
+      "c_common": [
+        "Drama"
       ],
       "h": 750000000,
       "i": "4e70ea06c092255ef70047cb",
@@ -144,13 +183,16 @@ window.onload = function () {
         "Psychological",
         "Supernatural"
       ],
+      "c_common": [
+        "Drama"
+      ],
       "h": 445261067,
       "i": "4e70ea07c092255ef7004809",
       "im": "16/16d7de04e67b014fa32299d8428dc174fa68222a18ec5195ef53b978.png",
       "ld": 1330718878.0,
       "s": 2,
       "t": "Petshop of Horrors"
-    },
+    },*/
     {
       "a": "kidou-senshi-gundam-climax-u.c",
       "c": [
@@ -159,6 +201,9 @@ window.onload = function () {
         "Mecha",
         "Sci-fi",
         "Shounen"
+      ],
+      "c_common": [
+        "Drama"
       ],
       "h": 182227182,
       "i": "4e70ea71c092255ef7006dcc",
@@ -169,49 +214,112 @@ window.onload = function () {
     }
   ]
 
-  /* @@@ fonction : calcul de la somme des hits @@@ */
-  var hitsSum = 0
-  for (var i = 0; i < mangas.length; i++) {
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* @@@ CONTANT & INITIALIZATION @@@ */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+  /*let w = d3.select("#suggestionContainer").node().getBoundingClientRect().width - 50*/
+  let w = 1068
+  let h = 800
+  let barPadding = 10
+  let aLotSimilar = "sun-flower"
+  let similar = "orange"
+  let verySimilar = "pumpkin"
+  /* calcul de la somme des hits */
+  let hitsSum = 0
+  for (let i = 0; i < mangas.length; i++) {
     hitsSum += mangas[i].h
   }
 
+  d3.select("#squareALotSimilar").classed("bg-" + aLotSimilar, true)
+  d3.select("#squareSimilar").classed("bg-" + similar, true)
+  d3.select("#squareVerySimilar").classed("bg-" + verySimilar, true)
+
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* @@@ FONCTIONS @@@ */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+  /* @@@ fonction : number formatter @@@ */
+  function abbreviateNumber(value) {
+    var newValue = value
+    if (value >= 1000) {
+      var suffixes = ["", "K", "M", "B", "T"]
+      var suffixNum = Math.floor(("" + value).length / 3)
+      var shortValue = ""
+      for (var precision = 2; precision >= 1; precision--) {
+        shortValue = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000, suffixNum) ) : value).toPrecision(precision))
+        var dotLessShortValue = (shortValue + "").replace(/[^a-zA-Z 0-9]+/g, "")
+        if (dotLessShortValue.length <= 2) {
+          break
+        }
+      }
+      if (shortValue % 1 !== 0) shortNum = shortValue.toFixed(1)
+      newValue = shortValue + suffixes[suffixNum]
+    }
+    return newValue
+  }
+
   /* @@@ fonction : over consequences @@@ */
-  var over = function (that, manga) {
+  let over = function (that, manga) {
     d3.select(that)
         .classed("over", true)
     tip.show(manga)
   }
 
   /* @@@ fonction : out consequences @@@ */
-  var out = function (that) {
+  let out = function (that) {
     d3.select(that)
         .classed("over", false)
     tip.hide()
   }
 
+  /* @@@ fonction : out consequences @@@ */
+  let getSimilarityColor = function (manga) {
+    let c_commonSize = manga.c_common.length
+    if (c_commonSize >= 1 && c_commonSize <= 2) {
+      return aLotSimilar
+    }
+    if (c_commonSize >= 3 && c_commonSize <= 4) {
+      return similar
+    }
+    return verySimilar
+  }
 
   /* @@@ fonction : calcul la hauteur d'une barre pour un manga selon ses hits @@@ */
-  var getBarHeight = function (manga) {
+  let getBarHeight = function (manga) {
     return ((manga.h) / hitsSum ) * 2000
   }
 
-  /* @@@ const @@@ */
-  var w = d3.select("main").node().getBoundingClientRect().width - 100
-  var h = 750
-  var barPadding = 10
 
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
   /* @@@ Crée l'élément SVG @@@ */
-  var svg = d3.select("#suggestionContainer")
-      .append("svg")
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+  let svg = d3.select("#suggestionContainer")
+      .insert("svg")
       .attr("width", w)
       .attr("height", h)
       .attr("class", "overflow")
 
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
   /* @@@ Ajout des barres selon les datas @@@ */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
   svg.selectAll("rect")
       .data(mangas)
       .enter()
       .append("rect")
+      .attr("class", function (manga, i) {
+        return "color-" + getSimilarityColor(manga)
+      })
       .attr("x", function (manga, i) {
         return i * (w / mangas.length)
       })
@@ -223,21 +331,45 @@ window.onload = function () {
         return getBarHeight(manga)
       })
 
-
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
   /* @@@ Création du tooltip @@@ */
-  var tip = d3.tip()
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+  let tip = d3.tip()
       .attr("class", "d3-tip")
       .offset([-10, 0])
       .html(function (manga) {
-        return "<strong>" + manga.t + "</strong>"
+        let similarityColor = getSimilarityColor(manga)
+        let res = "<div class='border border-" + similarityColor + " round bg-yang anim-zoom very-fast' style='min-width: 300px'>"
+        res += "<h4 class='margin-0 bg-" + similarityColor + " color-yang padding'>" + manga.t + "</h4>"
+        res += "<ul class='list-unstyled padding'>"
+        for (let category of manga.c) {
+          if ((manga.c_common.indexOf(category) > -1)) {
+            res += "<li class='color-" + similarityColor + " txt-b'>"
+          } else {
+            res += "<li>"
+          }
+          res += category + "</li>"
+        }
+        res += "</ul>"
+        res += "</div>"
+        return res
       })
 
   svg.call(tip)
 
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
   /* @@@ Ajout des affiches du manga au dessus des bars @@@ */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
   svg.selectAll("image").data(mangas)
       .enter()
       .append("svg:image")
+      .attr("class", "cursor-pointer")
       .attr("width", w / mangas.length - barPadding)
       .attr("height", 222)
       .attr("x", function (manga, i) {
@@ -265,5 +397,54 @@ window.onload = function () {
         alert("google")
       })
 
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* @@@ Ajout des scores de popularité @@@ */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+
+  svg.selectAll("viewsScore")
+      .data(mangas)
+      .enter()
+      .append("text")
+      .text(function (manga) {
+        return "~ " + abbreviateNumber(manga.h)
+      })
+      .attr("x", function (manga, i) {
+        return i * (w / mangas.length) + 5
+      })
+      .attr("y", function (manga) {
+        return (h - getBarHeight(manga)) - d3.select(this).node().getBoundingClientRect().height + 15
+      })
+  /* .attr("width", function (manga) {
+     let rectX = d3.transform(d3.select("rect").node()attr("transform")).translate[0]
+     console.log(rectX)
+     let recttW = d3.select("rect").node().getBoundingClientRect().width
+     let wText = this.getComputedTextLength()
+   })
+*/
+
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* @@@ Nombre de catégories en commun @@@ */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+  /* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: */
+
+  svg.selectAll("nbCommonCate")
+      .data(mangas)
+      .enter()
+      .append("text")
+      .text(function (manga) {
+        return manga.c_common.length
+      })
+      .attr("x", function (manga, i) {
+        return i * (w / mangas.length) + 5
+      })
+      .attr("y", function (manga) {
+        return (h - getBarHeight(manga)) - d3.select(this).node().getBoundingClientRect().height + 50
+      })
+      .attr("font-size", "32px")
+      .attr("fill", "white")
 
 }
